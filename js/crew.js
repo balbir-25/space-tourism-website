bb415045d0316ca83d29230d90d245bca2eba641
Balbir Singh <83856212+balbir-25@users.noreply.github.com>
2023-06-21 22:49:03 +0530

    Update crew.js

--- js/crew.js
+++ js/crew.js
@@ -16,7 +16,7 @@ crewNavItem.classList.add('active');
 
 // fetch data
 
-const list = fetch("https://balbir-25.github.io/repository/data.json").then((response) => {
+const list = fetch("https://balbir-25.github.io/space-tourism-website/data.json").then((response) => {
     return response.json();
 })
 
